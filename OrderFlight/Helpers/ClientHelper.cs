d75bda805b2942ea5ae7df89dfaef59702166b3d
GineTik <densheva18072003@gmail.com>
2022-11-11 20:24:41 +0200

    added Readme file and change some classes

---
+++ OrderFlight/Helpers/ClientHelper.cs
@@ -0,0 +1,44 @@
+﻿using OrderFlight.Models;
+
+namespace OrderFlight.Helpers
+{
+    public class ClientHelper
+    {
+        public static Client GetClient()
+        {
+            return new Client()
+            {
+                BoughtTickets = new(),
+                Cards = new()
+                {
+                    new Card()
+                    {
+                        Fullname = "Peter Parker",
+                        Number = "0000 0000 0000 0000",
+                        CVC = 616,
+                        ValidUntil = DateTime.Parse("11.11.2023"),
+                        Money = 5400,
+                    },
+
+                    new Card()
+                    {
+                        Fullname = "Peter Parker",
+                        Number = "1111 1111 1111 1111",
+                        CVC = 713,
+                        ValidUntil = DateTime.Parse("11.11.2023"),
+                        Money = 100,
+                    },
+
+                    new Card()
+                    {
+                        Fullname = "Peter Parker",
+                        Number = "1234 1234 1234 1234",
+                        CVC = 233,
+                        ValidUntil = DateTime.Parse("11.11.2025"),
+                        Money = 450,
+                    },
+                },
+            };
+        }
+    }
+}
--- OrderFlight/Models/Card.cs
+++ OrderFlight/Models/Card.cs
@@ -6,5 +6,6 @@
         public string Number { get; set; }
         public int CVC { get; set; }
         public DateTime ValidUntil { get; set; }
+        public decimal Money { get; set; }
     }
 }
