59e80376eea988ad6da521c69090506c3ed6e172
GineTik <densheva18072003@gmail.com>
2022-11-12 19:04:02 +0200

    added TicketService

---
+++ OrderFlight/Services/TicketService.cs
@@ -0,0 +1,27 @@
+﻿using OrderFlight.Models;
+
+namespace OrderFlight.Services
+{
+    public class TicketService
+    {
+        private readonly Client _client;
+
+        public TicketService(Client client) =>
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+
+        public void BuyTicket(Ticket ticket, Card card)
+        {
+            if (ticket == null)
+                throw new ArgumentNullException(nameof(ticket));
+
+            if (card == null)
+                throw new ArgumentNullException(nameof(card));
+
+            if (card.Money < ticket.Price)
+                throw new InvalidCastException("Not enought money");
+
+            card.Money = ticket.Price;
+            _client.BoughtTickets.Add(ticket);
+        }
+    }
+}
